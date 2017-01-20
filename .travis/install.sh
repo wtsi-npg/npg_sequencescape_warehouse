5de945519512e224cf7c46a40a3e8b99e0a4dd27
Marina Gourtovaia <mg8@sanger.ac.uk>
2017-01-20 11:37:18 +0000

    loader components are needed

--- .travis/install.sh
+++ .travis/install.sh
@@ -12,7 +12,7 @@ cpanm --quiet --notest Module::Build
 
 # WTSI NPG Perl repo dependencies
 repos=""
-for repo in perl-dnap-utilities perl-irods-wrap ml_warehouse npg_tracking npg_seq_common npg_qc; do
+for repo in perl-dnap-utilities perl-irods-wrap ml_warehouse npg_tracking npg_seq_common npg_qc npg_ml_warehouse; do
     cd /tmp
     # Always clone master when using depth 1 to get current tag
     git clone --branch master --depth 1 ${WTSI_NPG_GITHUB_URL}/${repo}.git ${repo}.git
